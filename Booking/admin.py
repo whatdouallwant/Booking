d56f3a0732304d1aacabfa940c11d6187f528234
Whynot <masya.babenko@gmail.com>
2025-08-13 23:50:12 +0300

    absolute cinema

--- Booking/admin.py
+++ Booking/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from Booking.models import Room, Bookings, Room_Image
+from Booking.models import Room, Bookings, Room_Image, Amenity
 
 admin.site.register(Room)
 admin.site.register(Bookings)
 admin.site.register(Room_Image)
+admin.site.register(Amenity)
--- Booking/models.py
+++ Booking/models.py
@@ -2,6 +2,12 @@ from turtle import mode
 from django.db import models
 from django.contrib.auth.models import User
 
+class Amenity(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
 class Room(models.Model):
     room_number = models.CharField(max_length=10, unique=True)
     room_description = models.TextField(blank=True, null=True)
@@ -12,9 +18,12 @@ class Room(models.Model):
     ]
     room_type = models.CharField(max_length=50, choices=TYPE_CHOICES)
     amount_of_beds = models.IntegerField(default=1)
-    max_guests = models.IntegerField(default=1)
+    min_guests = models.IntegerField(default=1)
+    max_guests = models.IntegerField(default=2)
     price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
     picture = models.ImageField(upload_to='room_pictures/', blank=True, null=True)
+    amenities = models.ManyToManyField(Amenity, blank=True)
+    area = models.DecimalField(max_digits=10, decimal_places=2, default=20)
 
     def __str__(self):
         return f"Room {self.room_number} - {self.room_type}"
@@ -50,10 +59,3 @@ class Bookings(models.Model):
         verbose_name = 'Бронювання'
         verbose_name_plural = 'Бронювання'
         ordering = ['-created_at']
-
-class Factors(models.Model):
-    wifi = models.BooleanField(default=False)
-    air_conditioning = models.BooleanField(default=False)
-    balcony = models.BooleanField(default=False)
-    tv = models.BooleanField(default=False)
-    breakfast_included = models.BooleanField(default=False)
