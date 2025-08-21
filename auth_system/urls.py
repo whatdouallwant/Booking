f335b98a21ae18106834536080bfd067428a5987
Whynot <masya.babenko@gmail.com>
2025-08-21 18:35:51 +0300

    bebee

--- auth_system/urls.py
+++ auth_system/urls.py
@@ -3,6 +3,6 @@ from auth_system import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
 ]
--- auth_system/views.py
+++ auth_system/views.py
@@ -14,7 +14,7 @@ def register(request):
         form = UserCreationForm()
     return render(request, template_name='auth_system/register.html', context={"form":form})
 
-def login(request):
+def user_login(request):
     form = AuthenticationForm()
     if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
@@ -24,7 +24,7 @@ def login(request):
             return redirect('home')
     return render(request, template_name='auth_system/login.html', context={"form":form})
 
-def logout(request):
+def user_logout(request):
     if request.user.is_authenticated:
         logout(request)
     return redirect('home')
